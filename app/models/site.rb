79afea7e9b5c1ff640904bde2270c13c7b46a6fd
WATANABE Hiroaki <hwat@mac.com>
2016-03-30 17:20:14 +0900

    identify article before update

--- app/models/site.rb
+++ app/models/site.rb
@@ -11,23 +11,37 @@ class Site < ActiveRecord::Base
       title: rss.channel.title,
       date: rss.channel.date,
       description: rss.channel.description
-      ).find_or_create_by(link: rss.channel.link)
+    ).find_or_create_by(link: rss.channel.link)
 
     rss.items.each do |item|
-      if !item.guid.content
-        logger.warn "item.guid is not presented"
-      else
-        Article.find_or_initialize_by(guid: item.guid.content).update(
-          channel_id: channel.id,
-          title: item.title,
-          link: item.link,
-          description: item.description,
-          author: item.author,
-          pubdate: item.pubDate,
-          permalink: item.guid.PermaLink?,
-          content: item.content_encoded
-          )
+      guid = item.try(:guid).try(:content)
+      guid = item.link unless guid
+      unless guid
+        raise "cannot detect the guid of an item on channel=[#{channel.id}]"
       end
+
+      article = Article.find_by(channel_id: channel.id, guid: guid)
+      if article
+        if article.same_as_item?(item)
+          logger.debug("article=[#{article.id}] on channel=[#{channel.id}] exists, skip")
+          next
+        end
+        logger.debug("article=[#{article.id}] on channel=[#{channel.id}] exists but modified, to update")
+      else
+        logger.debug("new article guid=[#{guid}]")
+        article = Article.new(channel_id: channel.id, guid: guid)
+      end
+
+      article.update(
+        title: item.title,
+        link: item.link,
+        description: item.description,
+        author: item.author,
+        pubdate: item.pubDate,
+        permalink: item.guid.try(:PermaLink?),
+        content: item.content_encoded
+      )
     end
+    self
   end
 end
