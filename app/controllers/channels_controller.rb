f6f024c0ff28a22d17538e75e37456f9d37a4b89
WATANABE Hiroaki <176736+hiroaki@users.noreply.github.com>
2024-08-06 20:54:26 +0900

    rails g scaffold Channel ...
    $ rails g scaffold Channel title:string link:string description:text last_build_date:datetime

---
+++ app/controllers/channels_controller.rb
@@ -0,0 +1,58 @@
+class ChannelsController < ApplicationController
+  before_action :set_channel, only: %i[ show edit update destroy ]
+
+  # GET /channels
+  def index
+    @channels = Channel.all
+  end
+
+  # GET /channels/1
+  def show
+  end
+
+  # GET /channels/new
+  def new
+    @channel = Channel.new
+  end
+
+  # GET /channels/1/edit
+  def edit
+  end
+
+  # POST /channels
+  def create
+    @channel = Channel.new(channel_params)
+
+    if @channel.save
+      redirect_to @channel, notice: "Channel was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /channels/1
+  def update
+    if @channel.update(channel_params)
+      redirect_to @channel, notice: "Channel was successfully updated.", status: :see_other
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /channels/1
+  def destroy
+    @channel.destroy!
+    redirect_to channels_url, notice: "Channel was successfully destroyed.", status: :see_other
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_channel
+      @channel = Channel.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def channel_params
+      params.require(:channel).permit(:title, :link, :description, :last_build_date)
+    end
+end
