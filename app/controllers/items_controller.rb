a18273aeff835d03b0e05cef98e677ecc25bd2e5
WATANABE Hiroaki <176736+hiroaki@users.noreply.github.com>
2024-08-08 21:13:41 +0900

    implement items controller as nested resource

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,10 @@
 class ItemsController < ApplicationController
+  before_action :set_channel
   before_action :set_item, only: %i[ show edit update destroy ]
 
   # GET /items
   def index
-    @items = Item.all
+    @items = @channel.items
   end
 
   # GET /items/1
@@ -12,7 +13,7 @@ class ItemsController < ApplicationController
 
   # GET /items/new
   def new
-    @item = Item.new
+    @item = @channel.items.build
   end
 
   # GET /items/1/edit
@@ -21,10 +22,10 @@ class ItemsController < ApplicationController
 
   # POST /items
   def create
-    @item = Item.new(item_params)
+    @item = @channel.items.build(item_params)
 
     if @item.save
-      redirect_to @item, notice: "Item was successfully created."
+      redirect_to [@channel, @item], notice: "Item was successfully created."
     else
       render :new, status: :unprocessable_entity
     end
@@ -33,7 +34,7 @@ class ItemsController < ApplicationController
   # PATCH/PUT /items/1
   def update
     if @item.update(item_params)
-      redirect_to @item, notice: "Item was successfully updated.", status: :see_other
+      redirect_to [@channel, @item], notice: "Item was successfully updated.", status: :see_other
     else
       render :edit, status: :unprocessable_entity
     end
@@ -42,17 +43,23 @@ class ItemsController < ApplicationController
   # DELETE /items/1
   def destroy
     @item.destroy!
-    redirect_to items_url, notice: "Item was successfully destroyed.", status: :see_other
+    redirect_to channel_items_url(@channel), notice: "Item was successfully destroyed.", status: :see_other
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+    def set_channel
+      @channel = Channel.find(params[:channel_id])
+    end
+
     def set_item
       @item = Item.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def item_params
-      params.fetch(:item, {})
+      permitted_item_params
+    end
+
+    def permitted_item_params
+      params.require(:item).permit(:title, :link, :description)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  resources :items
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -10,9 +9,9 @@ Rails.application.routes.draw do
   # root "posts#index"
 
   resources :channels do
+    resources :items
     member do
       get :fetch
-      get :items
     end
   end
 end
