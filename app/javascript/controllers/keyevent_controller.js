84fc16ea045ed7383bc2b5eab7ba8a3bb599ce57
WATANABE Hiroaki <176736+hiroaki@users.noreply.github.com>
2024-08-28 14:45:11 +0900

    implement extra keyevent actions on channels

--- app/javascript/controllers/keyevent_controller.js
+++ app/javascript/controllers/keyevent_controller.js
@@ -2,49 +2,40 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   connect() {
-    console.info("keyevent_controller")
+    console.info('keyevent_controller');
   }
 
-  cancelScrollByKeydown(evt) {
+  handlerKeydown(evt) {
     switch(evt.keyCode) {
       case 38:
       case 40:
+        // cancel window scrolling
         evt.preventDefault();
         evt.stopPropagation();
         break;
     }
   }
 
-  keyupEnter(evt) {
-    // console.info("keyevent_controller#keyup_enter", evt)
-    // console.info("keyevent_controller#keyup_enter", evt.target.dataset.url)
+  handlerKeyup(evt) {
     switch(evt.keyCode) {
       case 37:
         console.info('left');
         break;
       case 38:
         console.info('up');
-        if (this.focusPrevItem(evt.target)) {
-          // console.log('stopImmediatePropagation: '+ evt.cancelable);
-          // evt.preventDefault();
-          // evt.stopPropagation();
-          // evt.stopImmediatePropagation();
-        }
+        this.focusPrevItem(evt.target);
         break;
       case 39:
         console.info('right');
+        this.clickLinkForItems(evt.target);
         break;
       case 40:
         console.info('down');
-        if (this.focusNextItem(evt.target)) {
-          // console.log('stopImmediatePropagation: '+ evt.cancelable);
-          // evt.preventDefault();
-          // evt.stopPropagation();
-          // evt.stopImmediatePropagation();
-        }
+        this.focusNextItem(evt.target);
         break;
       case 13:
         console.info('enter');
+        this.openWindow(evt.target.dataset.url);
         break;
       case 8:
         console.info('delete');
@@ -55,41 +46,56 @@ export default class extends Controller {
     console.info('shiftKey: '+ evt.shiftKey);
   }
 
+  clickLinkForItems(elem) {
+    let a_tag = elem.querySelector('a[data-listen-keyevent="right"]');
+    if (a_tag) {
+      a_tag.click();
+    }
+  }
+
+  openWindow(url) {
+    return window.open(url, '_blank', 'noreferrer');
+  }
+
   focusNextItem(elem) {
-    let items = document.querySelectorAll("li[data-model='item']")
-    let len = items.length;
+    const items = document.querySelectorAll('li[data-model="item"]')
+    const len = items.length;
     let next_item = null;
-    for(var i =0; i < len; ++i ){
+
+    for (let i = 0; i < len; ++i) {
       if (items.item(i) == document.activeElement) {
         next_item = i + 1;
         break;
       }
     }
-    if (next_item != null && next_item < len ) {
-      items.item(next_item).focus();
-      return true;
+    if (next_item != null && next_item < len) {
+      elem = items.item(next_item)
+      elem.focus();
+      return elem;
     }
     else {
-      return false;
+      return null;
     }
   }
 
   focusPrevItem(elem) {
-    let items = document.querySelectorAll("li[data-model='item']")
-    let len = items.length;
+    const items = document.querySelectorAll('li[data-model="item"]')
+    const len = items.length;
     let prev_item = null;
-    for(var i =0; i < len; ++i ){
+
+    for (let i = 0; i < len; ++i) {
       if (items.item(i) == document.activeElement) {
         prev_item = i - 1;
         break;
       }
     }
     if (prev_item != null && 0 <= prev_item) {
-      items.item(prev_item).focus();
-      return true;
+      elem = items.item(prev_item)
+      elem.focus();
+      return elem;
     }
     else {
-      return false;
+      return null;
     }
   }
 }
