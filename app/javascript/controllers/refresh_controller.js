617c077b134a9a4c7b5baf95d8ea490ac9180967
WATANABE Hiroaki <176736+hiroaki@users.noreply.github.com>
2024-09-24 01:06:12 +0900

    promise-queue processing

--- app/javascript/controllers/refresh_controller.js
+++ app/javascript/controllers/refresh_controller.js
@@ -1,4 +1,5 @@
 import { Controller } from "@hotwired/stimulus"
+import Queue from "promise-queue"
 
 export default class extends Controller {
   connect() {
@@ -9,18 +10,32 @@ export default class extends Controller {
   all() {
     const channel_frames = document.getElementById('channels').querySelectorAll('turbo-frame');
 
+    const generate_promise = function(channel_frame) {
+      return function() {
+        return new Promise(async function(resolve, reject) {
+          console.log("["+ channel_frame.id +"] start");
+
+          const response = await Turbo.fetch(channel_frame.dataset['urlForRefresh'], {
+            method: 'PATCH',
+            headers: {
+              'X-CSRF-Token': document.querySelector('meta[name="csrf-token"]').content,
+              'Turbo-Frame': channel_frame.id,
+              'Accept': 'text/vnd.turbo-stream.html, text/html, application/xhtml+xml'
+            }
+          })
+
+          document.getElementById(channel_frame.id).delegate.loadResponse(new Turbo.FetchResponse(response));
+
+          console.log("["+ channel_frame.id +"] end");
+          resolve(response);
+        });
+      }
+    }
+
+    const que = new Queue(2);
+
     channel_frames.forEach(channel_frame => {
-      Turbo.fetch(channel_frame.dataset['urlForRefresh'], {
-        method: 'PATCH',
-        headers: {
-          'X-CSRF-Token': document.querySelector('meta[name="csrf-token"]').content,
-          'Turbo-Frame': channel_frame.id,
-          'Accept': 'text/vnd.turbo-stream.html, text/html, application/xhtml+xml'
-        }
-      })
-      .then((response) => {
-        document.getElementById(channel_frame.id).delegate.loadResponse(new Turbo.FetchResponse(response))
-      });
+      que.add(generate_promise(channel_frame));
     });
   }
 }
