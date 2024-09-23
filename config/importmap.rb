a24745936d5d43014eeb96252460bd1d5681f17b
WATANABE Hiroaki <176736+hiroaki@users.noreply.github.com>
2024-09-23 18:27:24 +0900

    importmap pin promise-queue
    $ ./bin/importmap pin promise-queue
    Pinning "promise-queue" to vendor/javascript/promise-queue.js via download from https://ga.jspm.io/npm:promise-queue@2.2.5/index.js
    Pinning "process" to vendor/javascript/process.js via download from https://ga.jspm.io/npm:@jspm/core@2.0.1/nodelibs/browser/process-production.js
    $

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,3 +5,5 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
+pin "promise-queue" # @2.2.5
+pin "process" # @2.0.1
