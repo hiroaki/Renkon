85a9f68d7017badb530840e66f7bef0e62384e73
WATANABE Hiroaki <176736+hiroaki@users.noreply.github.com>
2024-09-24 20:09:36 +0900

    improvement

--- app/javascript/controllers/refresh_controller.js
+++ app/javascript/controllers/refresh_controller.js
@@ -2,40 +2,47 @@ import { Controller } from "@hotwired/stimulus"
 import Queue from "promise-queue"
 
 export default class extends Controller {
+  static values = { concurrency: Number }
+
   connect() {
-    console.info('refresh_controller');
+    console.info('refresh_controller#connect');
+  }
+
+  initialize() {
+    console.info('refresh_controller#new');
+    this.concurrencyValue ||= 4;
+    console.log(`this.concurrencyValue=[${this.concurrencyValue}]`)
   }
 
   // refresh all channels
   all() {
-    const channel_frames = document.getElementById('channels').querySelectorAll('turbo-frame');
+    const generate_fetch_function = (url_for_refresh, frame_id) => {
+      return async () => {
+        console.log(`[${frame_id}] start ${url_for_refresh}`);
 
-    const generate_promise = function(channel_frame) {
-      return function() {
-        return new Promise(async function(resolve, reject) {
-          console.log("["+ channel_frame.id +"] start");
-
-          const response = await Turbo.fetch(channel_frame.dataset['urlForRefresh'], {
-            method: 'PATCH',
-            headers: {
-              'X-CSRF-Token': document.querySelector('meta[name="csrf-token"]').content,
-              'Turbo-Frame': channel_frame.id,
-              'Accept': 'text/vnd.turbo-stream.html, text/html, application/xhtml+xml'
-            }
-          })
-
-          document.getElementById(channel_frame.id).delegate.loadResponse(new Turbo.FetchResponse(response));
-
-          console.log("["+ channel_frame.id +"] end");
-          resolve(response);
+        const response = await Turbo.fetch(url_for_refresh, {
+          method: 'PATCH',
+          headers: {
+            'X-CSRF-Token': document.querySelector('meta[name="csrf-token"]').content,
+            'Turbo-Frame': frame_id,
+            'Accept': 'text/vnd.turbo-stream.html, text/html, application/xhtml+xml'
+          }
         });
+
+        console.log(`[${frame_id}] end`);
+        return response;
       }
-    }
+    };
 
-    const que = new Queue(2);
+    const que = new Queue(this.concurrencyValue);
 
-    channel_frames.forEach(channel_frame => {
-      que.add(generate_promise(channel_frame));
+    document.getElementById('channels').querySelectorAll('turbo-frame').forEach(channel_frame => {
+      que.add(
+        generate_fetch_function(channel_frame.dataset['urlForRefresh'], channel_frame.id)
+      )
+      .then((response) => {
+        document.getElementById(channel_frame.id).delegate.loadResponse(new Turbo.FetchResponse(response));
+      });
     });
   }
 }
