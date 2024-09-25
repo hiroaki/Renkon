9630ccd609546229cf899e33c7ca13680164f89f
WATANABE Hiroaki <176736+hiroaki@users.noreply.github.com>
2024-09-25 22:45:51 +0900

    use high level function instead of Turbo.fetch

--- app/javascript/controllers/refresh_controller.js
+++ app/javascript/controllers/refresh_controller.js
@@ -1,6 +1,26 @@
 import { Controller } from "@hotwired/stimulus"
 import Queue from "promise-queue"
 
+function _getMetaElement(name) {
+    return document.querySelector(`meta[name="${name}"]`);
+  }
+
+function _getMetaContent(name) {
+    const element = _getMetaElement(name);
+    return element && element.content;
+  }
+
+function _getCookieValue(cookieName) {
+    if (cookieName != null) {
+      const cookies = document.cookie ? document.cookie.split("; ") : []
+      const cookie = cookies.find((cookie) => cookie.startsWith(cookieName))
+      if (cookie) {
+        const value = cookie.split("=").slice(1).join("=")
+        return value ? decodeURIComponent(value) : undefined
+      }
+    }
+  }
+
 export default class extends Controller {
   static values = { concurrency: Number }
 
@@ -16,6 +36,64 @@ export default class extends Controller {
 
   // refresh all channels
   all() {
+
+    class RefreshDelegator {
+      constructor(location, method, target_frame_id) {
+        this.location = location
+        this.method = method
+        this.target_frame_id = target_frame_id;
+        this.fetchRequest = new Turbo.FetchRequest(this, this.method, this.location, new URLSearchParams(), this.target_frame_id)
+      }
+
+      perform() {
+        this.fetchRequest.perform()
+      }
+
+      // (1)
+      prepareRequest(request) {
+        console.log("prepareRequest")
+        if (!request.isSafe) {
+          const token = _getCookieValue(_getMetaContent("csrf-param")) || _getMetaContent("csrf-token")
+          if (token) {
+            request.headers["X-CSRF-Token"] = token
+          }
+        }
+      }
+
+      // (2)
+      requestStarted(_request) {
+        console.log("requestStarted")
+      }
+
+      requestPreventedHandlingResponse(request, response) {
+        console.log("requestPreventedHandlingResponse")
+      }
+
+      // (3)
+      requestSucceededWithResponse(request, response) {
+        console.log("requestSucceededWithResponse")
+        document.getElementById(this.target_frame_id).delegate.loadResponse(response)
+      }
+
+      requestFailedWithResponse(request, response) {
+        console.log("requestFailedWithResponse")
+      }
+
+      requestErrored(request, error) {
+        console.log("requestErrored")
+      }
+
+      // (4)
+      requestFinished(_request) {
+        console.log("requestFinished")
+      }
+
+    }
+
+    (new RefreshDelegator("/channels/3/fetch", "PATCH", "channel_3")).perform()
+  }
+
+  _all() {
     const generate_fetch_function = (url_for_refresh, frame_id) => {
       return async () => {
         console.log(`[${frame_id}] start ${url_for_refresh}`);
@@ -45,4 +123,5 @@ export default class extends Controller {
       });
     });
   }
+
 }
