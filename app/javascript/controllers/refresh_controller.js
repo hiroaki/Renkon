239e3da4cdac3015be1c2c5c5f080bd5b2d424ad
WATANABE Hiroaki <176736+hiroaki@users.noreply.github.com>
2024-09-27 12:59:30 +0900

    improvement

--- app/javascript/controllers/refresh_controller.js
+++ app/javascript/controllers/refresh_controller.js
@@ -1,25 +1,14 @@
 import { Controller } from "@hotwired/stimulus"
 import Queue from "promise-queue"
+import TurboFrameDelegator from "lib/turbo_frame_delegator"
 
-function _getMetaElement(name) {
-    return document.querySelector(`meta[name="${name}"]`);
+class RefreshChanelsDelegator extends TurboFrameDelegator {
+  // override
+  prepareRequest(request) {
+    super.prepareRequest(request)
+    console.log("request", request)
   }
-
-function _getMetaContent(name) {
-    const element = _getMetaElement(name);
-    return element && element.content;
-  }
-
-function _getCookieValue(cookieName) {
-    if (cookieName != null) {
-      const cookies = document.cookie ? document.cookie.split("; ") : []
-      const cookie = cookies.find((cookie) => cookie.startsWith(cookieName))
-      if (cookie) {
-        const value = cookie.split("=").slice(1).join("=")
-        return value ? decodeURIComponent(value) : undefined
-      }
-    }
-  }
+}
 
 export default class extends Controller {
   static values = { concurrency: Number }
@@ -36,70 +25,30 @@ export default class extends Controller {
 
   // refresh all channels
   all() {
-
-    class RefreshDelegator {
-      constructor(location, method, target_frame_id) {
-        this.location = location
-        this.method = method
-        this.target_frame_id = target_frame_id;
-        this.fetchRequest = new Turbo.FetchRequest(this, this.method, this.location, new URLSearchParams(), this.target_frame_id)
-      }
-
-      perform() {
-        this.fetchRequest.perform()
-      }
-
-      // (1)
-      prepareRequest(request) {
-        console.log("prepareRequest")
-        if (!request.isSafe) {
-          const token = _getCookieValue(_getMetaContent("csrf-param")) || _getMetaContent("csrf-token")
-          if (token) {
-            request.headers["X-CSRF-Token"] = token
-          }
-        }
-      }
-
-      // (2)
-      requestStarted(_request) {
-        console.log("requestStarted")
-      }
-
-      requestPreventedHandlingResponse(request, response) {
-        console.log("requestPreventedHandlingResponse")
-      }
-
-      // (3)
-      requestSucceededWithResponse(request, response) {
-        console.log("requestSucceededWithResponse")
-        document.getElementById(this.target_frame_id).delegate.loadResponse(response)
-      }
-
-      requestFailedWithResponse(request, response) {
-        console.log("requestFailedWithResponse")
-      }
-
-      requestErrored(request, error) {
-        console.log("requestErrored")
-      }
-
-      // (4)
-      requestFinished(_request) {
-        console.log("requestFinished")
-      }
-
+    const generate_fetch_function = (url_for_refresh, method, frame_id) => {
+      return async () => new RefreshChanelsDelegator(url_for_refresh, method, frame_id).perform();
     }
 
-    (new RefreshDelegator("/channels/3/fetch", "PATCH", "channel_3")).perform()
+    const que = new Queue(this.concurrencyValue);
+    document.getElementById('channels').querySelectorAll('turbo-frame').forEach(channel_frame => {
+      que.add(
+        generate_fetch_function(
+          channel_frame.dataset['urlForRefresh'],
+          channel_frame.dataset['method'],
+          channel_frame.id
+        )
+      )
+    });
   }
 
+  // 試作：低レベル関数 Turbo.fetch を利用したもの
   _all() {
-    const generate_fetch_function = (url_for_refresh, frame_id) => {
+    const generate_fetch_function = (url_for_refresh, method, frame_id) => {
       return async () => {
         console.log(`[${frame_id}] start ${url_for_refresh}`);
 
         const response = await Turbo.fetch(url_for_refresh, {
-          method: 'PATCH',
+          method: method,
           headers: {
             'X-CSRF-Token': document.querySelector('meta[name="csrf-token"]').content,
             'Turbo-Frame': frame_id,
@@ -116,12 +65,15 @@ export default class extends Controller {
 
     document.getElementById('channels').querySelectorAll('turbo-frame').forEach(channel_frame => {
       que.add(
-        generate_fetch_function(channel_frame.dataset['urlForRefresh'], channel_frame.id)
+        generate_fetch_function(
+          channel_frame.dataset['urlForRefresh'],
+          channel_frame.dataset['method'],
+          channel_frame.id
+        )
       )
       .then((response) => {
         document.getElementById(channel_frame.id).delegate.loadResponse(new Turbo.FetchResponse(response));
       });
     });
   }
-
 }
--- config/importmap.rb
+++ config/importmap.rb
@@ -7,3 +7,4 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "promise-queue" # @2.2.5
 pin "process" # @2.0.1
+pin_all_from "app/javascript/lib", under: "lib"
