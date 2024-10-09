4a7878fc23a317ed26dafa9bc01e523cde8d8d86
WATANABE Hiroaki <176736+hiroaki@users.noreply.github.com>
2024-10-09 20:32:34 +0900

    key right on channel pane

--- app/javascript/controllers/pane_focus_controller.js
+++ app/javascript/controllers/pane_focus_controller.js
@@ -15,9 +15,55 @@ export default class extends Controller {
   }
 
   // keyup LEFT on items pane
-  backToChannel(evt) {
-    const selectedChannel = document.getElementById('channels').querySelector('li[data-selected="true"]');
+  backToChannelPane(evt) {
     this.focusPane(this.paneTargets[0]);
+
+    const selectedChannel = document.getElementById('channels').querySelector('li[data-selected="true"]');
     selectedChannel.focus();
+  }
+
+  // keyup RIGHT on channels pane
+  forwardToItemPane(evt) {
+    this.focusPane(this.paneTargets[1]);
+
+    const selectedItems = document.getElementById('items').querySelectorAll('li[data-selected="true"]');
+    if (0 < selectedItems.length) {
+      selectedItems.item(selectedItems.length - 1).focus();
+    } else {
+
+      const li = document.getElementById('items').querySelectorAll('li').item(0);
+      if (li) {
+        // this.enterItem(li);
+        li.focus(); // Important for being the base point for next and previous
+        const aTag = li.getElementsByTagName('A').item(0);
+        aTag.click(); // This is going to invoke changeSelected()
+      }
+    }
+  }
+
+  // keyup SPACE on channels pane
+  forwardToUnreadItemPane(evt) {
+    console.log("forwardToUnreadItemPane", evt)
+    this.focusPane(this.paneTargets[1]);
+
+    const items = document.getElementById('items').querySelectorAll('li');
+    let pos = -1;
+    for (let i = 0; i < items.length; ++i) {
+      if (items[i] == evt.currentTarget) {
+        pos = i;
+        break;
+      }
+    }
+
+    for (let i = pos + 1; i < items.length; ++i) {
+      let li = items[i];
+      if (li.dataset['unread'] == 'true') {
+        // this.enterItem(items[i]);
+        li.focus(); // Important for being the base point for next and previous
+        const aTag = li.getElementsByTagName('A').item(0);
+        aTag.click(); // This is going to invoke changeSelected()
+        break;
+      }
+    }
   }
 }
