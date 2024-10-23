de9903f65d8ee86a7a7402f68ce84c04b9e944cb
WATANABE Hiroaki <176736+hiroaki@users.noreply.github.com>
2024-10-23 13:53:19 +0900

    enable channel to delete on backspace

--- app/javascript/controllers/channels_controller.js
+++ app/javascript/controllers/channels_controller.js
@@ -1,4 +1,32 @@
 import SelectedLiBaseController from "lib/selected_li_base_controller"
 
 export default class extends SelectedLiBaseController {
+  confirmDestroy(evt) {
+    const message = evt.currentTarget.dataset['textForConfirmDestroy'] || 'Sure?';
+    if (!confirm(message)) {
+      evt.stopImmediatePropagation()
+    }
+  }
+
+  destroyChannel(evt) {
+    const me = this;
+    const li = evt.currentTarget;
+    const url = li.dataset['urlDestroy'];
+
+    return fetch(url, {
+      method: 'DELETE',
+      headers: {
+        'X-CSRF-Token': document.querySelector('meta[name=csrf-token]').content
+      }
+    })
+    .then(response => {
+      if (response.ok) {
+        li.remove();
+      }
+      else {
+        console.error('Failed to delete the channel', response);
+      }
+    })
+    .catch(error => console.error('Error:', error));
+  }
 }
