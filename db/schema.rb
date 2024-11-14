a0aa881773ac276b9f7ae99e05cc970fd3b7d60a
WATANABE Hiroaki <176736+hiroaki@users.noreply.github.com>
2024-11-14 20:35:25 +0900

    ./bin/rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_11_06_100106) do
+ActiveRecord::Schema[8.0].define(version: 2024_11_14_110051) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -61,6 +61,13 @@ ActiveRecord::Schema[7.2].define(version: 2024_11_06_100106) do
     t.integer "channel_id", null: false
     t.boolean "disabled", default: false, null: false
     t.index ["channel_id"], name: "index_items_on_channel_id"
+  end
+
+  create_table "memos", force: :cascade do |t|
+    t.string "title"
+    t.datetime "touched_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
