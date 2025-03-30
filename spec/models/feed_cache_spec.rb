7e65da12f0c3dd85c6ed03beb9820116d52e71bb
WATANABE Hiroaki <176736+hiroaki@users.noreply.github.com>
2025-03-30 23:18:50 +0900

    add spec for model FeedCache

--- spec/models/feed_cache_spec.rb
+++ spec/models/feed_cache_spec.rb
@@ -10,5 +10,33 @@ RSpec.describe FeedCache, type: :model do
 end
 
 RSpec.describe FeedCache, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:subscription) { FactoryBot.create(:subscription) }
+  let(:feed_cache) { FactoryBot.create(:feed_cache, subscription: subscription) }
+
+  describe 'validations' do
+    it 'is valid with valid attributes' do
+      expect(feed_cache).to be_valid
+    end
+
+    it 'is not valid without a subscription_id' do
+      feed_cache.subscription_id = nil
+      expect(feed_cache).not_to be_valid
+    end
+
+    it 'is valid without contents' do
+      feed_cache.contents = nil
+      expect(feed_cache).to be_valid
+    end
+
+    it 'is valid without cached_at' do
+      feed_cache.cached_at = nil
+      expect(feed_cache).to be_valid
+    end
+  end
+
+  describe 'associations' do
+    it 'belongs to a subscription' do
+      expect(feed_cache).to respond_to(:subscription)
+    end
+  end
 end
