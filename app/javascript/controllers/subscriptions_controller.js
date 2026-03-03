e66773db55080d5d143043fceab17f085a8dbb08
WATANABE Hiroaki <176736+hiroaki@users.noreply.github.com>
2026-03-04 03:26:05 +0900

    Fix method name for firing selection change event in subscriptions controller

--- app/javascript/controllers/subscriptions_controller.js
+++ app/javascript/controllers/subscriptions_controller.js
@@ -20,7 +20,7 @@ class RefreshSubscriptionDelegator extends TurboFrameDelegator {
 export default class extends SelectedLiBaseController {
   connect() {
     super.connect();
-    this.fireChangeSelectedLiEvent(this.element, this.getSelectedItem());
+    this.fireSelectionChanged(this.getSelectedItem());
   }
 
   // Subscription の削除処理の前提として、この確認の動作を発動させるイベントに続いて、
