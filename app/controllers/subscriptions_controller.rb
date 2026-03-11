9fc4f7ad2d131ef6af0e477334195e2e73576322
WATANABE Hiroaki <176736+hiroaki@users.noreply.github.com>
2026-03-11 23:16:21 +0900

    Rename subscription fetch action to refresh_feed

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -1,7 +1,7 @@
 class SubscriptionsController < ApplicationController
   include Factory
 
-  before_action :set_subscription, only: %i[ edit update destroy fetch ]
+  before_action :set_subscription, only: %i[ edit update destroy refresh_feed ]
 
   # FOR DEVELOPMENT
   def main
@@ -93,8 +93,8 @@ class SubscriptionsController < ApplicationController
     end
   end
 
-  # fetch_subscription PATCH /subscriptions/:id/fetch(.:format)
-  def fetch
+  # refresh_feed_subscription PATCH /subscriptions/:id/refresh_feed(.:format)
+  def refresh_feed
     logger.info("params[:dry_run]=[#{params[:dry_run] ? 'true' : 'false'}]")
     unless params[:dry_run]
       fetch_and_merge_feed_entries_for_subscription(@subscription)
@@ -102,7 +102,7 @@ class SubscriptionsController < ApplicationController
 
     redirect_to subscription_url(@subscription, short: !!params[:short]), notice: "Subscription was successfully refreshed.", status: :see_other
   rescue FeedUtils::Error => error
-    render_fetch_error(error)
+    render_refresh_feed_error(error)
   end
 
   # reorder_tree_subscriptions PATCH /subscriptions/reorder_tree(.:format)
@@ -143,7 +143,7 @@ class SubscriptionsController < ApplicationController
       render json: { error: message }, status: :unprocessable_content
     end
 
-    def render_fetch_error(error)
+    def render_refresh_feed_error(error)
       if params[:short]
         render json: {
           error: error.message,
--- config/routes.rb
+++ config/routes.rb
@@ -33,7 +33,7 @@ Rails.application.routes.draw do
     end
 
     member do
-      patch :fetch
+      patch :refresh_feed
     end
   end
 
