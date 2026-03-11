209dd293e16973f9c23416b74c41b77b9d32009c
WATANABE Hiroaki <176736+hiroaki@users.noreply.github.com>
2026-03-11 12:31:22 +0900

    bin/rails app:update (override all)

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,5 +1,4 @@
 require "active_support/core_ext/integer/time"
-require_relative "../../lib/middleware/short_request_id" # [Renkon APPENDS]
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
@@ -47,6 +46,12 @@ Rails.application.configure do
   # Highlight code that enqueued background job in logs.
   config.active_job.verbose_enqueue_logs = true
 
+  # Highlight code that triggered redirect in logs.
+  config.action_dispatch.verbose_redirect_logs = true
+
+  # Suppress logger output for asset requests.
+  config.assets.quiet = true
+
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
 
@@ -55,11 +60,4 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions.
   config.action_controller.raise_on_missing_callback_actions = true
-
-  # [Renkon APPENDS]
-  config.middleware.insert_before Rails::Rack::Logger, Middleware::ShortRequestId
-  config.log_tags = [->(req) { req.env["X_SHORT_REQUEST_ID"] || "no-id" }]
-
-  # [Renkon APPENDS]
-  config.active_storage.service = :local
 end
