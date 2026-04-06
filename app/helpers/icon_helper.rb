7612bf121871e6f546f9fd645eb0a8a8be80852d
WATANABE Hiroaki <176736+hiroaki@users.noreply.github.com>
2026-04-06 20:02:05 +0900

    Adjust subscription group row layout

--- app/helpers/icon_helper.rb
+++ app/helpers/icon_helper.rb
@@ -37,6 +37,12 @@ module IconHelper
     end
   end
 
+  def icon_folder(css_class: 'size-4', **opts)
+    content_tag(:svg, xmlns: 'http://www.w3.org/2000/svg', fill: 'none', viewBox: '0 0 24 24', 'stroke-width': '1.5', stroke: 'currentColor', class: css_class, 'aria-hidden': 'true', focusable: 'false', **opts) do
+      tag.path(nil, 'stroke-linecap' => 'round', 'stroke-linejoin' => 'round', d: 'M2.25 12.75V12A2.25 2.25 0 0 1 4.5 9.75h15A2.25 2.25 0 0 1 21.75 12v.75m-8.69-6.44-2.12-2.12a1.5 1.5 0 0 0-1.061-.44H4.5A2.25 2.25 0 0 0 2.25 6v12a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18V9a2.25 2.25 0 0 0-2.25-2.25h-5.379a1.5 1.5 0 0 1-1.06-.44Z')
+    end
+  end
+
   def icon_new_group(css_class: 'size-6', **opts)
     content_tag(:svg, xmlns: 'http://www.w3.org/2000/svg', fill: 'none', viewBox: '0 0 24 24', 'stroke-width': '1.5', stroke: 'currentColor', class: css_class, 'aria-hidden': 'true', focusable: 'false', **opts) do
       tag.path(nil, 'stroke-linecap' => 'round', 'stroke-linejoin' => 'round', d: 'M12 10.5v6m3-3H9m4.06-7.19-2.12-2.12a1.5 1.5 0 0 0-1.061-.44H4.5A2.25 2.25 0 0 0 2.25 6v12a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18V9a2.25 2.25 0 0 0-2.25-2.25h-5.379a1.5 1.5 0 0 1-1.06-.44Z')
